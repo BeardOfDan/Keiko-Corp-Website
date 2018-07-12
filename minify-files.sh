af76fb384f8e29bdac3abf1fd98ed686bde37314
Daniel Roberts <BeardOfDan@gmail.com>
2018-07-11 19:33:51 -0700

    (feat) Add outline of bash script to minify files

---
+++ minify-files.sh
@@ -0,0 +1,19 @@
+!#/bin/sh
+  
+# check if the current branch's contents have been commited
+# if so, then proceed
+# else, tell the user that they need to wrap things up, then exit the script
+
+# check the current git branch
+
+# if minified, then proceed
+# else, switch to minified
+
+# copy the contents of master into minify
+
+# minify all the js and css files
+
+# make a commit
+# perhaps try to get data from master branch log and use that for the commit message?
+
+# echo to the user if it worked
