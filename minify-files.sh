d4e214849ee05254d51caf2b930ffa60620f8f0b
Daniel Roberts <BeardOfDan@gmail.com>
2018-07-13 01:47:44 -0700

    (feat) Finish outline of minify script

--- minify-files.sh
+++ minify-files.sh
@@ -13,10 +13,9 @@
 # else, tell the user that it is a dependency of the script, then exit the script
 
 # check the current git branch
-#   git rev-parse --abbrev-ref HEAD
+#   git rev-parse --abbrev-ref HEAD # name of current branch
 # if minified, then proceed
 # else, switch to minified, then proceed
-#   copy the contents of master into minify
 #   git checkout minified
 #   git checkout master . #copy contents of master into minified
 
@@ -41,3 +40,17 @@
 #   Check the minified commit history, use that to check against the master branch's commit history (specifically, the commit messages).
 #   Make sure that the body of the minified commit message has a listing of all of the header messages for the master branch's commit history
 #   The header of the commit message for the minified branch might be a number/date combo (ex. commit #15, 01/23/18 @ 17:53). In the start of the body, before it includes the main branch's notes, should include hash for the main branch's commit that it is based on.
+
+# git rev-parse HEAD # gets hash of current commit
+# git rev-parse --short HEAD # gets SHORT hash of current commit
+
+
+
+
+
+
+
+
+
+
+
