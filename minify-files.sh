d68a638b06715b54e05afafe7090b80bed1f2f31
Daniel Roberts <BeardOfDan@gmail.com>
2018-07-21 12:20:52 -0700

    (documentation) Remove excess comments from minification script
    There were a lot of comments left over from the outline

--- minify-files.sh
+++ minify-files.sh
@@ -5,8 +5,6 @@ echo -e   '===============\n';
 
 # check if the git branch is clean
 gitStatus=$(git status --porcelain);
-# echo -en '\ngit status: '; echo $gitStatus # should be an empty string, if not, then is not clean
-# note: when this script has been altered, it will stop the status from being clean
 
 if [[ "$gitStatus" != '' ]] 
 then
@@ -18,10 +16,6 @@ fi
 
 # check if the dependencies (uglifyjs and uglifycss) are installed
 # uglifyjs --version => 'uglify-es 3.3.9'
-#   if the first few characters are 'uglify-es' then it is installed
-#   Future todo: check that the installed version is at least the current 
-#    (as of this script's writing) version
-#echo -en '\nuglify version: '; echo $(uglifyjs --version);
 uglifyVersion=$(uglifyjs --version);
 uglifyStart='uglify-es'; # the start of uglify version
 
@@ -32,12 +26,6 @@ then
   exit 1;
 
 fi
-
-# check the current git branch
-# echo -en '\ncurrent git branch: '; echo $(git rev-parse --abbrev-ref HEAD);
-# handle branch switching if needed
-#   ex. if on master, (which is probable), then checkout minified
-#                                          git checkout minified
 
 currentBranch=$(git rev-parse --abbrev-ref HEAD);
 currentShortHash=$(git rev-parse --short master);
@@ -71,9 +59,6 @@ then
 fi
 
 # copy master's contents into minified
-#   git checkout master . 
-#echo -e '\ngit checkout master .';
-
 git checkout master .
 
 echo -e '\nFiles to be minified:';
@@ -86,19 +71,11 @@ find public/js -type f \
   -exec rm {} \; \
   -exec mv {}.min {} \;
 
-# commit changes
-# need to have generated commit message
-#echo -e "\ngit commit -am 'Generated commit message'";
-
 git commit -am "branch: $currentBranch | short hash: $currentShortHash"
 
 # push updated minify branch
 #echo -e '\ngit push productionServerRemote minifiedBranch';
 echo 'TODO: add line to push to server'
-
-# go back to the branch the user was on (presumably master)
-#   otherwise, they'll have to do this step themselves everytime they use this script
-#echo -e '\ngit checkout master';
 
 git checkout $currentBranch
 
